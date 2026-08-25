12a1e2a52c1c5655fec692e2d18f53b29b3fa3a8
Leonardo Kessy <leokessy2005@gmail.com>
2026-08-25 14:14:26 -0300

    feat(domain): build client domain entity

--- src/main/java/org/arquiweb/store/domain/models/Client.java
+++ src/main/java/org/arquiweb/store/domain/models/Client.java
@@ -3,8 +3,31 @@ package org.arquiweb.store.domain.models;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.UUID;
+
 @Getter
 @AllArgsConstructor
 public class Client {
-    // TODO - Constructor, setters
+    private UUID id;
+    private String name;
+    private String email;
+
+    public Client(String name, String email) {
+        this.id = UUID.ofEpochMillis(System.currentTimeMillis()); // Generate UUID v7
+        this.name = name;
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Name cannot be null or blank");
+        }
+        this.name = name;
+    }
+    public void setEmail(String email) {
+        if (email == null || email.isBlank()) {
+            throw new IllegalArgumentException("Email cannot be null or blank");
+        }
+        this.email = email;
+    }
 }
