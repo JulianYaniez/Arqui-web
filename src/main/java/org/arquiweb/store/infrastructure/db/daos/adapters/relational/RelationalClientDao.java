3f034201bf0e1b74f71e41b8fec6859123a40eba
Julian Yañez <julianfidel06@gmail.com>
2026-08-28 15:48:59 -0300

    feat(dao): methods findById and findAll of clientDao fixed

--- src/main/java/org/arquiweb/store/infrastructure/db/daos/adapters/relational/RelationalClientDao.java
+++ src/main/java/org/arquiweb/store/infrastructure/db/daos/adapters/relational/RelationalClientDao.java
@@ -42,6 +42,7 @@ public class RelationalClientDao extends DaoAdapter implements ClientRepository 
 
             if(rs.next()) {
                 Client client = new Client(
+                        (UUID) rs.getObject("id"),
                         rs.getString("name"),
                         rs.getString("email")
                 );
@@ -56,7 +57,11 @@ public class RelationalClientDao extends DaoAdapter implements ClientRepository 
 
    public List<Client>  findAll() {
        List<Client> res = new ArrayList<>();
-       String sql = "SELECT * FROM " + this.table;
+       String sql = "SELECT * FROM " + this.table  + """
+                    JOIN invoices i USING(clientId)
+                    ORDER BY count(i.clientId) DESC
+                   """; // REVISAR (Se necesita el valor de la tabla producto).
+
        try(
                Connection conn = db.getConnection();
                PreparedStatement stmt = conn.prepareStatement(sql);
@@ -64,6 +69,7 @@ public class RelationalClientDao extends DaoAdapter implements ClientRepository 
        ) {
            while(rs.next()) {
                Client client = new Client(
+                       (UUID) rs.getObject("id"),
                        rs.getString("name"),
                        rs.getString("email")
                );
--- src/main/java/org/arquiweb/store/infrastructure/db/daos/adapters/relational/RelationalInvoiceDao.java
+++ src/main/java/org/arquiweb/store/infrastructure/db/daos/adapters/relational/RelationalInvoiceDao.java
@@ -6,12 +6,17 @@ import org.arquiweb.store.infrastructure.db.daos.adapters.DaoAdapter;
 import org.arquiweb.store.infrastructure.db.engines.Database;
 import org.arquiweb.store.infrastructure.db.engines.DatabaseFactory;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public class RelationalInvoiceDao extends DaoAdapter implements InvoiceRepository {
 
+    private final String table = "invoices";
     private static RelationalInvoiceDao instance;
 
     private RelationalInvoiceDao(Database db) {
@@ -26,8 +31,23 @@ public class RelationalInvoiceDao extends DaoAdapter implements InvoiceRepositor
     }
 
     public Optional<Invoice> findById(UUID id) {
-        // TODO
-        return null;
+        String sql = "SELECT * FROM " + this.table + " WHERE id = ?";
+        try(
+                Connection conn = db.getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql);
+        ) {
+            stmt.setObject(1, id);
+            ResultSet rs = stmt.executeQuery();
+
+            if(rs.next()) {
+                Invoice invoice = new Invoice( (UUID) rs.getObject("id"));
+
+                return Optional.of(invoice);
+            }
+        } catch (SQLException e) {
+            System.err.println("Customer not found : " + e.getMessage());
+        }
+        return Optional.empty();
     }
 
     public List<Invoice>  findAll() {
