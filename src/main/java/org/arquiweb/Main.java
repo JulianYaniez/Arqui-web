489969eba321b5632bed611547992246b2402c62
Leonardo Kessy <leokessy2005@gmail.com>
2026-08-29 18:05:56 -0300

    chore(main): remove unutilized imports on main

--- src/main/java/org/arquiweb/Main.java
+++ src/main/java/org/arquiweb/Main.java
@@ -1,19 +1,7 @@
 package org.arquiweb;
 
-import org.arquiweb.store.application.ports.factories.DaoFactory;
-import org.arquiweb.store.application.ports.repositories.ClientRepository;
-import org.arquiweb.store.application.ports.repositories.InvoiceProductRepository;
-import org.arquiweb.store.application.ports.repositories.InvoiceRepository;
-import org.arquiweb.store.application.ports.repositories.ProductRepository;
-import org.arquiweb.store.infrastructure.db.daos.factories.RelationalDaoFactory;
-import org.arquiweb.store.infrastructure.db.init.builders.RelationalBuilder;
 import org.arquiweb.store.infrastructure.db.init.initializers.Initializer;
 import org.arquiweb.store.infrastructure.db.init.initializers.RelationalInitializer;
-import org.arquiweb.store.infrastructure.db.init.readers.CsvReader;
-import org.arquiweb.store.infrastructure.db.init.seeders.ClientsSeeder;
-import org.arquiweb.store.infrastructure.db.init.seeders.InvoiceProductsSeeder;
-import org.arquiweb.store.infrastructure.db.init.seeders.InvoicesSeeder;
-import org.arquiweb.store.infrastructure.db.init.seeders.ProductsSeeder;
 
 public class Main {
     static void main() {
