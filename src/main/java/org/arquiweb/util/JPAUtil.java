c65d62160c05174b5aeab018cbe8aa7fee5ec598
Leonardo Kessy <leokessy2005@gmail.com>
2026-09-09 23:13:56 -0300

    chore(jpa): build utility class to get entity manager

---
+++ src/main/java/org/arquiweb/util/JPAUtil.java
@@ -0,0 +1,13 @@
+package org.arquiweb.util;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JPAUtil {
+    public static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("group-7");
+
+    public static EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+}
